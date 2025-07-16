6237c8a63c7a20ab3e7d86047959323c85057166
Muhammad Sameer <76097339+sameerdata@users.noreply.github.com>
2025-07-16 15:29:23 +0500

    Update deploy_to_sagemaker.py

--- deploy_to_sagemaker.py
+++ deploy_to_sagemaker.py
@@ -18,11 +18,12 @@ s3_path = f"s3://{bucket}/models/{model_file}"
 container = {
     'Image': '683313688378.dkr.ecr.us-east-1.amazonaws.com/sagemaker-scikit-learn:0.20.0-cpu-py3',
     'ModelDataUrl': s3_path,
-        'Environment': {
-        'SAGEMAKER_PROGRAM': 'inference.py',
+    'Environment': {
+        'SAGEMAKER_PROGRAM': 'code/inference.py',
         'SAGEMAKER_SUBMIT_DIRECTORY': s3_path
     }
 }
+
 
 # Initialize client
 sagemaker = boto3.client('sagemaker', region_name=region)
